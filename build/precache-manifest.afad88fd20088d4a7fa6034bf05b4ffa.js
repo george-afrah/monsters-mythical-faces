99cd1e3661d65b13fa11a58d9c0d37e72595331e
George.afrah <george.afrah@gmail.com>
2019-07-13 20:56:36 +0200

    add monster cards static page

---
+++ build/precache-manifest.afad88fd20088d4a7fa6034bf05b4ffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679c1eeb934dc446543edc413c04a9d6",
+    "url": "/monsters-mythical-faces/index.html"
+  },
+  {
+    "revision": "5ddc9a88fb93231cb0ba",
+    "url": "/monsters-mythical-faces/static/css/main.3d803ded.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-mythical-faces/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5ddc9a88fb93231cb0ba",
+    "url": "/monsters-mythical-faces/static/js/main.7afd3aa0.chunk.js"
+  },
+  {
+    "revision": "24921ba11681c9827165",
+    "url": "/monsters-mythical-faces/static/js/runtime~main.94679a66.js"
+  }
+]);
